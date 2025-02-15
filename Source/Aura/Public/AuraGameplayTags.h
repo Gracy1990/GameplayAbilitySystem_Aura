c59691ac300e0bd7c487c2ea5d67ad8e2ffcc8a9
Mfon Enoh Grace <gracyenoh@gmail.com>
2025-02-15 18:36:03 +0100

    Aura Asset Manager

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,26 @@
+// Copyright Maniform Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ * 
+ * Singleton containinf native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Armor;
+
+protected: 
+
+private:
+	static FAuraGameplayTags GameplayTags;
+};
