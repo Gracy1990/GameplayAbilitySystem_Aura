ede6c6bab34b20028188e644be13d86e16f845f5
Mfon Enoh Grace <gracyenoh@gmail.com>
2025-02-24 09:55:39 +0100

    Ability Damage

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -120,4 +120,10 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FString("Input Tag for 4 Key")
 	);
 
+	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Damage"),
+		FString("Damage")
+	);
+
+
 }
