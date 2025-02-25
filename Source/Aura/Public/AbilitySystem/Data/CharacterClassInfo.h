2cb71f0911d9223eba5669e92e14c73d820c81ef
Mfon Enoh Grace <gracyenoh@gmail.com>
2025-02-25 13:20:14 +0100

    Damage Calculation Coefficients

--- Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -44,7 +44,10 @@ public:
 	TSubclassOf<UGameplayEffect> VitalAttributes;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
-	TArray < TSubclassOf<UGameplayAbility>> CombatAbilities;
+	TArray < TSubclassOf<UGameplayAbility>> CommonAbilities;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults|Damage")
+	TObjectPtr<UCurveTable> DamageCalculationCoefficients;
 
 	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
 
