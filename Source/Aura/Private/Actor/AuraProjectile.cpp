ff2e115d3cc08b0abf7673a8b838c7e115f36955
Mfon Enoh Grace <gracyenoh@gmail.com>
2025-03-10 10:07:47 +0100

    Rock Impact Effects

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -1,6 +1,5 @@
 // Copyright Maniform Studio
 
-
 #include "Actor/AuraProjectile.h"
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemBlueprintLibrary.h"
@@ -12,7 +11,6 @@
 #include "Components/AudioComponent.h"
 #include "Aura/Aura.h"
 #include "AuraAbilitySystemLibrary.h"
-
 
 AAuraProjectile::AAuraProjectile()
 {
@@ -33,7 +31,6 @@ AAuraProjectile::AAuraProjectile()
 	ProjectileMovement->MaxSpeed = 550.f;
 	ProjectileMovement->ProjectileGravityScale = 0.f;
 }
-
 void AAuraProjectile::BeginPlay()
 {
 	Super::BeginPlay();
@@ -44,7 +41,6 @@ void AAuraProjectile::BeginPlay()
 		LoopingSoundComponent = UGameplayStatics::SpawnSoundAttached(LoopingSound, GetRootComponent());
 	}
 }
-
 void AAuraProjectile::Destroyed()
 {
 	if (!bHit && !HasAuthority())
@@ -61,10 +57,12 @@ void AAuraProjectile::Destroyed()
 		{
 			LoopingSoundComponent->Stop();
 		}
+		{
+			bHit = true;
+		}
 	}
 	Super::Destroyed();
 }
-
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 										UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
@@ -81,6 +79,7 @@ void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, 
 		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
 		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
 		if (LoopingSoundComponent) LoopingSoundComponent->Stop();
+		bHit = true;
 	}
 
 	if (HasAuthority())
