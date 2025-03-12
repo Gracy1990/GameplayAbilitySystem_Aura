16cdb3d5756b0e70ce9a39ea8a55622625c2ab66
Mfon Enoh Grace <gracyenoh@gmail.com>
2025-03-12 07:24:08 +0100

    Elementalist Attack Task

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -75,4 +75,6 @@ public:
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	int32 GetMinionCount();
 
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void  IncrementMinionCount(int32 Amount);
 };
