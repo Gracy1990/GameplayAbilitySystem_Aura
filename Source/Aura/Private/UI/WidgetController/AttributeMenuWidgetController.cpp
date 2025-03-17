63d4ac8db28ed882b8794359ebebf07229b4f6bf
Mfon Enoh Grace <gracyenoh@gmail.com>
2025-03-17 20:15:41 +0100

    Attribute Points Member Variable

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -4,6 +4,7 @@
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "AbilitySystem/Data/AttributeInfo.h"
+#include "Player/AuraPlayerState.h"
 
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
@@ -12,14 +13,22 @@ void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 	check(AttributeInfo);
 	for (auto& Pair : AS->TagsToAttributes)
 	{
-	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
-		[this, Pair](const FOnAttributeChangeData& Data) 
-		{
-			BroadcastAttributeInfo(Pair.Key, Pair.Value());
-		}
-	);
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
+			[this, Pair](const FOnAttributeChangeData& Data)
+			{
+				BroadcastAttributeInfo(Pair.Key, Pair.Value());
+			}
+		);
 	}
 
+	AAuraPlayerState* AuraPlayerState = CastChecked<AAuraPlayerState>(PlayerState);
+	AuraPlayerState->OnAttributePointsChangedDelegate.AddLambda(
+		[this](int32 Points) 
+		{
+			AttributePointsChangedDelegate.Broadcast(Points);
+		}
+	
+	);
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
