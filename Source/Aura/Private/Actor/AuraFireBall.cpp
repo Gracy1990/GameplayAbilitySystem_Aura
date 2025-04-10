40c3e4807a8c0d7d6d8521ce17451a92f35b2572
Mfon Enoh Grace <gracyenoh@gmail.com>
2025-04-10 10:35:09 +0100

    Causing FireBall Damage

--- Source/Aura/Private/Actor/AuraFireBall.cpp
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Actor/AuraFireBall.h"
+#include "AuraAbilitySystemComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
 void AAuraFireBall::BeginPlay()
 {
@@ -9,7 +11,23 @@ void AAuraFireBall::BeginPlay()
 	StartOutgoingTimeline();
 }
 
-void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActer, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (!IsValidOverlap(OtherActor)) return;
 
+	if (HasAuthority())
+	{
+
+		if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor))
+		{
+			
+			const FVector DeathImpulse = GetActorForwardVector() * DamageEffectParams.DeathImpulseMagnitude;
+			DamageEffectParams.DeathImpulse = DeathImpulse;
+			const bool bKnockback = FMath::RandRange(1, 100) < DamageEffectParams.KnockbackChance;
+				
+			DamageEffectParams.TargetAbilitySystemComponent = TargetASC;
+			UAuraAbilitySystemLibrary::ApplyDamageEffect(DamageEffectParams);
+		
+		}
+	}
 }
