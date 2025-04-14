b2760c62472ef120238a335a16b24b4e7c1cd6f8
Mfon Enoh Grace <gracyenoh@gmail.com>
2025-04-14 15:17:21 +0100

    Checkpoint

---
+++ Source/Aura/Public/Checkpoint/Checkpoint.h
@@ -0,0 +1,39 @@
+// Copyright Maniform Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "GameFramework/DefaultPawn.h"
+#include "Checkpoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ACheckpoint : public APlayerStart
+{
+	GENERATED_BODY()
+public:
+	ACheckpoint(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActer, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void CheckpointReached(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	void HandleGlowEffects();
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+	
+};
